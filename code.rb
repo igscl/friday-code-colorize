4548879cc31dbad865e2153b6ede0a430009bf74
Ignacio Giadach <igiadach@gmail.com>
2019-10-25 09:59:05 +1100

    better code with colorize

--- code.rb
+++ code.rb
@@ -1,24 +1,25 @@
+require 'colorize'
 puts "Hey there! Welcome, please tell me your name"
 name = gets.chomp.to_s
 puts "hello #{name}, nice to see you here, what would you like
 to do?\n
     1. print the name of my favourite food\n
     2. exit\n
-    Please only use numbers"
+    Please only use numbers\n\n"
 choice = gets.chomp.to_i
 if choice == 1
-    puts "italian"
+    puts "Italian \n \n" .red
 else
-    puts "see you!"
+    puts "see you! \r"
 end
 puts "Hi again #{name}, what would you like
 to do now?\n
     1. print the name of my favourite drink\n
     2. exit\n
-    Please only use numbers"
+    Please only use numbers\n\n"
 choice = gets.chomp.to_i
 if choice == 1
-    puts "lemon juice"
+    puts "lemon juice \n \n" .yellow
 else
     puts "see you!"
 end
@@ -27,15 +28,15 @@ puts "Hi again #{name}, what would you like
 to do now?\n
     1. print the name of my favourite colour\n
     2. exit\n
-    Please only use numbers"
+    Please only use numbers\n\n"
 choice = gets.chomp.to_i
 if choice == 1
-    puts "light blue"
+    puts "light blue \n \n" .blue
 else
     puts "see you!"
 end
 puts "Now we will choose a random country for you,
-press any key to continue"
+press any key to continue\n\n"
 require 'io/console'                                                                                                       
 def continue_story                                                                                                               
                                                                                                    
@@ -43,6 +44,6 @@ def continue_story
   print "            \r" # extra space to overwrite in case next sentence is short                                                                                                              
 end  
 continue_story
-@countries = ["Canada", "Denmark", "Germany", "Isle of Man", "Namibia", "Qatar", "South Africa",  "United Kingdom","United States"]
+@countries = ["Canada", "Denmark", "Germany", "Chile", "Namibia", "Qatar", "South Africa",  "United Kingdom","United States"]
 random_country = @countries.sample
-puts "#{random_country}"
+puts "#{random_country}".green
